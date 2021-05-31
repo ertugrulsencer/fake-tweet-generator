4f6debf1a3ba21304c8e09c742a02d6ea7d1c23b
ertugrulsencer <teknoertugrul@gmail.com>
2021-05-31 20:44:19 +0300

    Header component added

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <main>
+    <Header />
+  </main>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import Header from "./components/Header";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Header,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
+main {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="header">
+    <strong class="logo">Fake Tweet Generator</strong>
+    <ul class="languages">
+      <li>Türkçe</li>
+      <li>English</li>
+    </ul>
+    <a href="javascript:return false;" class="share">Paylaş</a>
+  </header>
+</template>
+
+<script>
+export default {
+  props: {
+    langList: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+header.header {
+  height: 54px;
+}
+</style>
